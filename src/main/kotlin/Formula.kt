64ee9c281889695484dc0366a5db07b7785730dc
nevermindgabreuz <gabrieldallemole21@gmail.com>
2022-02-08 09:30:51 -0300

    terceiro

--- src/main/kotlin/Formula.kt
+++ src/main/kotlin/Formula.kt
@@ -2,20 +2,38 @@ import kotlin.math.sqrt
 
 class Formula {
 
-    fun acharForcaPeso(massa:Double,gravidade:Double):Double = massa * gravidade
+    fun acharForcaPeso(massa: Double, gravidade: Double): Double = massa * gravidade
 
-    fun forcaCentripeda(massa:Double,velocidade:Double,raio:Double):Double = massa * (sqrt(velocidade) /raio)
+    fun forcaCentripeda(massa: Double, velocidade: Double, raio: Double): Double = massa * (sqrt(velocidade) / raio)
 
-    fun impuslso(intensidade:Double,velocidadeInicial:Double, velocidadeFinal:Double,tempoInicial:Double,tempoFinal:Double ):Double
-    = intensidade * (tempoFinal - tempoInicial)
+    fun impuslso(
+        intensidade: Double,
+        velocidadeInicial: Double,
+        velocidadeFinal: Double,
+        tempoInicial: Double,
+        tempoFinal: Double
+    ): Double = intensidade * (tempoFinal - tempoInicial)
 
-    fun calcularForcaElastica(deformacao:Double, constanteElastica:Double):Double = deformacao * constanteElastica
+    fun calcularForcaElastica(deformacao: Double, constanteElastica: Double): Double = deformacao * constanteElastica
 
-    fun calcularVelocidadeMedia(posicaoFinal:Double,posicaoInicial:Double,tempoInicial:Double,tempoFinal:Double):Double
-    = (posicaoFinal - posicaoInicial) / (tempoFinal - tempoInicial)
+    fun calcularVelocidadeMedia(
+        posicaoFinal: Double,
+        posicaoInicial: Double,
+        tempoInicial: Double,
+        tempoFinal: Double
+    ): Double = (posicaoFinal - posicaoInicial) / (tempoFinal - tempoInicial)
 
-    fun calcularMovimentoUniforme(posicaoInicial:Double,velocidade:Double,tempoInicial: Double, tempoFinal: Double, tempoInicial: Double):
-    Double = posicaoInicial + velocidade * (tempoFinal - tempoInicial)
+    fun calcularMovimentoUniforme(posicaoInicial: Double, velocidade: Double, tempoInicial: Double, tempoFinal: Double):
+            Double = posicaoInicial + velocidade * (tempoFinal - tempoInicial)
 
-    //fun calcularMovimentoUniformementeVariavel()
+    fun calcularMovimentoUniformementeVariavel(
+        velocidadeInicial: Double,
+        velocidadeFinal: Double,
+        posicaoInicial: Double,
+        aceleracao: Double,
+        tempoInicial: Double,
+        tempoFinal: Double
+    ):
+            Double =
+        posicaoInicial + velocidadeInicial * (tempoFinal - tempoInicial) + (aceleracao * sqrt(tempoFinal - tempoInicial) / 2)
 }
